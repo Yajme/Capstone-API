1ba1be3e5bbed209a490a76472ccd776e1681841
Yajme <spartan.emjayrongavilla@gmail.com>
2024-05-10 23:35:23 +0800

    Updated Database

--- src/Model/aquaguard.sql
+++ src/Model/aquaguard.sql
@@ -1,33 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 06:21 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `db_aquaguard`
---
 CREATE DATABASE IF NOT EXISTS `db_aquaguard` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `db_aquaguard`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_cage`
---
 
 CREATE TABLE `tbl_cage` (
   `id` int(11) NOT NULL,
@@ -37,11 +18,9 @@ CREATE TABLE `tbl_cage` (
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_cage_status`
---
+INSERT INTO `tbl_cage` (`id`, `device_id`, `name`, `description`, `status`) VALUES
+(1, 0, 'CAGE_1', 'Near Shore', 'Active'),
+(2, 1, 'CAGE_2', 'Middle', 'Active');
 
 CREATE TABLE `tbl_cage_status` (
   `id` int(11) NOT NULL,
@@ -52,11 +31,9 @@ CREATE TABLE `tbl_cage_status` (
   `DO` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_contamination`
---
+INSERT INTO `tbl_cage_status` (`id`, `cage_id`, `ph_level`, `temperature`, `nitrate`, `DO`) VALUES
+(1, 1, 7.30, 23.30, 10.00, 95.00),
+(2, 2, 6.90, 29.30, 12.40, 89.32);
 
 CREATE TABLE `tbl_contamination` (
   `id` int(11) NOT NULL,
@@ -67,12 +44,6 @@ CREATE TABLE `tbl_contamination` (
   `link` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_device`
---
-
 CREATE TABLE `tbl_device` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -80,11 +51,9 @@ CREATE TABLE `tbl_device` (
   `status` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_maintenance`
---
+INSERT INTO `tbl_device` (`id`, `name`, `description`, `status`) VALUES
+(0, 'IOT_1', 'With Built in Sensors', 'Active'),
+(1, 'IOT_2', 'With Built in Sensors', 'Active');
 
 CREATE TABLE `tbl_maintenance` (
   `id` int(11) NOT NULL,
@@ -95,12 +64,6 @@ CREATE TABLE `tbl_maintenance` (
   `link` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_processed_records`
---
-
 CREATE TABLE `tbl_processed_records` (
   `id` int(11) NOT NULL,
   `cage_id` int(11) NOT NULL,
@@ -110,12 +73,6 @@ CREATE TABLE `tbl_processed_records` (
   `last_viewed_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_user_account`
---
-
 CREATE TABLE `tbl_user_account` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -123,11 +80,8 @@ CREATE TABLE `tbl_user_account` (
   `salt` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_user_info`
---
+INSERT INTO `tbl_user_account` (`id`, `username`, `password`, `salt`) VALUES
+(1, 'admin', '219f0c7597aa7bfc485b5a6dccd014dc58d1feb4c5ab935eebe99e67176f3a79', '44b46107bb');
 
 CREATE TABLE `tbl_user_info` (
   `id` int(11) NOT NULL,
@@ -139,45 +93,27 @@ CREATE TABLE `tbl_user_info` (
   `phone` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
+INSERT INTO `tbl_user_info` (`id`, `user_acc_id`, `fname`, `lname`, `mname`, `address`, `phone`) VALUES
+(1, 1, 'allenster', 'layvilla', 'null', 'talisay, santo tomas, laguna', '09295404374');
 
---
--- Indexes for table `tbl_cage`
---
+
 ALTER TABLE `tbl_cage`
   ADD PRIMARY KEY (`id`),
   ADD KEY `device_id` (`device_id`);
 
---
--- Indexes for table `tbl_cage_status`
---
 ALTER TABLE `tbl_cage_status`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cage_id` (`cage_id`);
 
---
--- Indexes for table `tbl_contamination`
---
 ALTER TABLE `tbl_contamination`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tbl_device`
---
 ALTER TABLE `tbl_device`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tbl_maintenance`
---
 ALTER TABLE `tbl_maintenance`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tbl_processed_records`
---
 ALTER TABLE `tbl_processed_records`
   ADD PRIMARY KEY (`id`),
   ADD KEY `contamination_level` (`contamination_level`),
@@ -185,47 +121,26 @@ ALTER TABLE `tbl_processed_records`
   ADD KEY `last_viewed_by` (`last_viewed_by`),
   ADD KEY `cage_id` (`cage_id`);
 
---
--- Indexes for table `tbl_user_account`
---
 ALTER TABLE `tbl_user_account`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `tbl_user_info`
---
 ALTER TABLE `tbl_user_info`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_acc_id` (`user_acc_id`);
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `tbl_cage`
---
 ALTER TABLE `tbl_cage`
   ADD CONSTRAINT `tbl_cage_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `tbl_device` (`id`);
 
---
--- Constraints for table `tbl_cage_status`
---
 ALTER TABLE `tbl_cage_status`
   ADD CONSTRAINT `tbl_cage_status_ibfk_1` FOREIGN KEY (`cage_id`) REFERENCES `tbl_cage` (`id`);
 
---
--- Constraints for table `tbl_processed_records`
---
 ALTER TABLE `tbl_processed_records`
   ADD CONSTRAINT `tbl_processed_records_ibfk_1` FOREIGN KEY (`contamination_level`) REFERENCES `tbl_contamination` (`id`),
   ADD CONSTRAINT `tbl_processed_records_ibfk_2` FOREIGN KEY (`maintenance_type`) REFERENCES `tbl_maintenance` (`id`),
   ADD CONSTRAINT `tbl_processed_records_ibfk_3` FOREIGN KEY (`last_viewed_by`) REFERENCES `tbl_user_account` (`id`),
   ADD CONSTRAINT `tbl_processed_records_ibfk_4` FOREIGN KEY (`cage_id`) REFERENCES `tbl_cage` (`id`);
 
---
--- Constraints for table `tbl_user_info`
---
 ALTER TABLE `tbl_user_info`
   ADD CONSTRAINT `tbl_user_info_ibfk_1` FOREIGN KEY (`user_acc_id`) REFERENCES `tbl_user_account` (`id`);
 COMMIT;
